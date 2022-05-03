47de69a98dde230a7139c65746acf0c36f47e2a4
TMSamuel <103134041+TMSamuel@users.noreply.github.com>
2022-05-03 10:35:45 +0200

    Add files via upload

--- EjPropuestos/AreaCilindro.py
+++ EjPropuestos/AreaCilindro.py
@@ -1,6 +1,11 @@
 # Area y Volumen de un cilindro
 import math
-Rd=1
-Ho=5
-pi=math.pi
-print(f"El Area del Cilindro es:",(pi*2)*Rd*Ho+(pi*2)*(Rd*Rd),"y el Volumen del Cilindro es:",pi*(Rd*Rd)*Ho)
+Rd=input("Introduce el Radio: ")
+Ho=input("Introduce el Ho: ")
+if(Rd.isdigit() and Ho.isdigit()):
+    Rd=int(Rd)
+    Ho=int(Ho)
+    pi=math.pi
+    print(f"El Area del Cilindro es:",(pi*2)*Rd*Ho+(pi*2)*(Rd*Rd),"y el Volumen del Cilindro es:",pi*(Rd*Rd)*Ho)
+else:
+    print("Introduce un numero valido")
